4f4265ba5b8b6fad98f188ead73493d57e730baf
fgraml17 <55577247+fgraml17@users.noreply.github.com>
2020-05-26 13:26:57 +0200

    OptionsMenu erstellt

--- app/src/main/java/net/htlgrieskirchen/smartf1/MainActivity.java
+++ app/src/main/java/net/htlgrieskirchen/smartf1/MainActivity.java
@@ -3,12 +3,30 @@ package net.htlgrieskirchen.smartf1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MenuItem Mcurrent_championships;
+    private MenuItem Mpast_championships;
+    private MenuItem Mrace_calendar;
+    private MenuItem Msettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        Mcurrent_championships = menu.findItem(R.id.current_championship);
+        Mpast_championships = menu.findItem(R.id.past_championship);
+        Mrace_calendar = menu.findItem(R.id.race_calendar);
+        Msettings = menu.findItem(R.id.settings);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
